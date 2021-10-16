6a6f92574402776ce4dd57f02eb83cba046fbe4e
tlaraine <tlaraine@student.21-school.ru>
2021-10-16 20:10:17 +0300

    add_getwd

--- srcs/check.cpp
+++ srcs/check.cpp
@@ -1,6 +1,14 @@
 #include "Server.hpp"
+#include "unistd.h"
+#include "limits.h"  //Для PATH_MAX
 
 int main (void)
 {
-	Server x("/Users/megagosha/42/webserv/webserv_config_example");
+    //GetCurrentDirectory()
+    char PathName[PATH_MAX];
+    getwd(PathName);
+    std::string c_path = (std::string)PathName + "/webserv_config_example";
+    Server x(c_path);
+
+	//Server x("/Users/mac/CLionProjects/webserv/webserv_config_example");
 }
