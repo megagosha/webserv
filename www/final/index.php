19c27f98ec217021260dec4ed65c1c111e52efcf
George Tevosov <megagosha@Georges-MacBook-Pro.local>
2021-11-19 18:27:58 +0300

    fix

--- www/final/index.php
+++ www/final/index.php
@@ -1,10 +1,11 @@
 <?php
 // phpinfo();
 // header("HTTP/1.1 404 Not Found");
-header('Content-type: image/png', true);
+//header('Content-type: image/png', true);
 // exit;
 //
-// // phpinfo();
-$entityBody = file_get_contents('php://input');
-echo($entityBody);
+echo ("12345");
+// phpinfo();
+//$entityBody = file_get_contents('php://input');
+//echo($entityBody);
 // echo(1);
